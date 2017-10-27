a4b9804db26217313fd589abf34f0965355fd412
nonoike <ij4nu8d4fw@gmail.com>
2017-10-27 22:26:02 +0900

    refactor: not use list.head

--- src/main/scala/com/s99/P08.scala
+++ src/main/scala/com/s99/P08.scala
@@ -4,7 +4,7 @@ object P08 {
   def compress(list: List[Any]): List[Any] = list match {
     case Nil => Nil
     case x :: Nil => x :: Nil
-    case x :: xs if (x == xs.head) => compress(xs)
-    case x :: xs if (x != xs.head) => x :: compress(xs)
+    case x1 :: x2 :: xs if (x1 == x2) => compress(x2 :: xs)
+    case x :: xs => x :: compress(xs)
   }
 }
